01a204cfd93de37cd7b70661f43894d4c9ab178e
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2025-08-21 09:07:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -52,21 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const sonidoCorrcto = new Audio("/audios/correcto.mp3");
     const sonidoIncorrecto = new Audio("/audios/incorrecto.mp3");
 
-  / Dentro de tu archivo app.js
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // La ruta correcta INCLUYE el nombre del repositorio
-    navigator.serviceWorker.register('/languagesolutionsa2/service-worker.js')
-      .then(registration => {
-        console.log('✅ ¡Service Worker registrado correctamente!');
-        console.log('Scope:', registration.scope); // Esto te dirá el alcance del SW
-      })
-      .catch(err => {
-        // Este error te dará la pista definitiva
-        console.error('❌ Fallo en el registro del Service Worker:', err);
-      });
-  });
-}
+    console.log("Service Worker file loaded!");
 
     // ---- FUNCIONES DE NAVEGACIÓN Y LÓGICA DE LA APLICACIÓN ----
     function ocultarTodasLasPantallas() {
